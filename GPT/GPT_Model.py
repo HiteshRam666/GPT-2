83bf6dbabe8d72f50beaebfb543a5007cf5f8fa6
hitesh <hiteshram321@gmail.com>
2025-07-15 19:21:32 +0530

    Model

--- GPT/GPT_Model.py
+++ GPT/GPT_Model.py
@@ -2,16 +2,6 @@ import torch
 import torch.nn as nn
 torch.manual_seed(123)
 
-GPT_CONFIG_124M = {
-    "vocab_size": 50257,    # Vocabulary size
-    "context_length": 256, # Context length
-    "emb_dim": 768,         # Embedding dimension
-    "n_heads": 12,          # Number of attention heads
-    "n_layers": 12,         # Number of layers
-    "drop_rate": 0.1,       # Dropout rate
-    "qkv_bias": False       # Query-Key-Value bias
-}
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
         super().__init__()
